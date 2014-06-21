35f68f2859fefcf3150c427810093be9f0c9ff7d
Mike Fielden <fielden.mike@gmail.com>
2014-06-21 14:09:50 -0500

    Adding in some actual functionality

--- index.js
+++ index.js
@@ -3,23 +3,60 @@
 var Backbone = require('Backbone');
 var Handlebars = require('Handlebars');
 
+var defaultOptions = {
+	type: 'info',
+	speed: 'fast'
+};
+
+var styleClasses = {
+	types: {
+		'info': 'info',
+		'success': 'success',
+		'warning': 'warning',
+		'error': 'error'
+	},
+	animationSpeeds: {
+		'fast': 'ani-fast',
+		'normal': 'ani-medium',
+		'slow': 'ani-slow'
+	},
+}
+
 var notificationView = Backbone.View.extend({
 	template: function () {
 		return [
-		'<h4 class="error-heading">Header here</h4>',
-		'<p class="lead error-body">Body of message here</p>'
+		'<h4 class="note-heading">{{header}}</h4>',
+		'<p class="lead note-body">{{message}}</p>',
+		'<button type="button" class="close" aria-hidden="true">&times;</button>'
 		].join('');
 	},
 
 	className: 'alert row-fluid col-lg-8 col-lg-offset-2 ' +
-	 ' col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 ' +
-	 ' fadeInDown ani-fast error-container',
+	 'col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 ' +
+	 'fadeInDown note-container',
 
 	events: {
+		'click button.close': 'closeNotification'
 	},
 
 	initialize: function (options) {
-		this.options = options;
+		var speed, type;
+
+		if (options === undefined) options = {};
+
+		this.options = _.defaults(options, defaultOptions);
+
+		// Add the right classes based on options
+		speed = styleClasses.animationSpeeds[this.options.speed];
+		type = styleClasses.types[this.options.type];
+
+		if (speed) {
+			this.$el.addClass(speed);
+		}
+
+		if (type) {
+			this.$el.addClass(type);
+		}
 
 		this.render();
 	},
@@ -28,6 +65,15 @@ var notificationView = Backbone.View.extend({
 		this.$el.append(Handlebars.compile(this.template())(this.options));
 
 		return this;
+	},
+
+	closeNotification: function (evt) {
+		evt.preventDefault();
+
+		this.trigger('close');
+
+		this.off();
+		this.remove();
 	}
 });
 
